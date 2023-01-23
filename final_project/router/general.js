f6a1a2a51ec2c05032a9b7179398136e4c4e90d4
Diego Sanchez <ds_keane@hotmail.com>
2023-01-24 00:59:07 +0100

    user regiser added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,32 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doExist = (username) => {
+  let userswithname = users.filter((user) => {
+    return user.username === username;
+  });
+
+  if(userswithname.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  if(username && password){
+    if(!doExist(username)){
+      users.push({"username":username, "password":password});
+      return res.status(200).json({message: "User successfully registered. Now you can login" })
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
